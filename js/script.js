d0aef630bf7a43a2c91570e0b30659c8e7b3df99
mosangin <75051063+mosangin@users.noreply.github.com>
2021-03-24 22:04:45 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,205 +1,77 @@
 $(function(){
-   var banner=$(".left_banner1>li");
-   var leftBtn=$(".pop_ctrl .prev");
-   var rightBtn=$(".pop_ctrl .next");
-   var banners=$(".right_banner1>li");
-   var leftBtns=$(".pop_ctrl2 .prev2");
-   var rightBtns=$(".pop_ctrl2 .next2");
-   var num=$('.count > span');
-   var num2=$('.page > span');
-   var box5=$(".box5_img>li")
-   var boxleft=$(".control .prev")
-   var boxright=$(".control .next")
-   var current=0;
-   var current1=0;
-   var current2=0;
-   var setIntervalId;
-   var setIntervalId2;
-    
-	/* left_banner */
-    function move(tg, start, end){
-      tg.css("left",start).stop().animate({left:end});
-    }
-    timer();
-   function timer(){
-      setIntervalId=setInterval(function(){
-        var prev=banner.eq(current);
-		var prevNum=num.eq(current);
-
-        move(prev,0,"-100%");
-		prevNum.removeClass('on');
-        current++;  
-        if(current==banner.size()){current=0;}
-        var next=banner.eq(current);
-		var nextNum=num.eq(current);
+    var pull    = $('.fas');
+      menu    = $('.var');
+      menuHeight  = menu.height();
    
-        move(next,'100%',0);
-		nextNum.addClass('on');
-      },2000);
-    }
-	var btn=true;
-	var play=$('.pop_ctrl .stop');
-	play.click(function(){
-		if(btn==true){
-            clearInterval(setIntervalId);
-			$(this).attr('Class','start')
-			btn=false;
-		}else{
-			timer();
-			$(this).attr('Class','stop')
-			btn=true;
-		}
+    $(pull).on('click', function(e) {
+      e.preventDefault();
+      menu.slideToggle();
     });
-	
-	$(".pop_ctrl .prev, .pop_ctrl .next ").hover(function(){
-		 clearInterval(setIntervalId);
-	},function(){
-		 timer();
-	});
-	
-    rightBtn.click(function(){
-		var prev=banner.eq(current);
-		var prevNum=num.eq(current);
+    $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 768 && menu.is(':hidden')) {
+          menu.removeAttr('style');
+        }
+      });
 
-        move(prev,0,"-100%");
-		prevNum.removeClass('on');
-        current++;  
-        if(current==banner.size()){current=0;}
-        var next=banner.eq(current);
-		var nextNum=num.eq(current);
-   
-        move(next,'100%',0);
-		nextNum.addClass('on');
-   });
-   
-   leftBtn.click(function(){
-		var prev=banner.eq(current);
-		var prevNum=num.eq(current);
-		move(prev,0,'100%');
-		prevNum.removeClass('on');
-		current--;
+      //맨 위로 올라가기
+      var sa = 700;
+			//스크롤 기본폼 window기억!
+			$(window).scroll(function(){
+				var num = $('html,body').scrollTop();
+				console.log(num);
+				if(num>sa){
+			   		$('.scroll_show').fadeIn();
+			   }else{
+			   		$('.scroll_show').fadeOut();
+			   }
+			});
+			$('.top').click(function(e){
+				$('html,body').stop().animate({scrollTop:0},1000,'swing');
+				e.preventDefault();
+			})
+     
+      //스크롤
       
-		if(current==-banner.size()){current=0;}
-		var next=banner.eq(current);
-		var nextNum=num.eq(current);
-		move(next,'-100%',0);  
-		nextNum.addClass('on');
+      $('#nav a, .var a').click(function(){
+      var $elem = $($(this).attr("href"));
+      var offset = $elem.offset().top - ($(window).height() / 2) + ($elem.height() / 2);
+      $('html, body').animate({
+        scrollTop: offset
+      }, 400);
+      return false;
     });
-	
-	/* right_banner */
-    var btn=true;
-    var play2=$('.pop_ctrl2 .stop2');
-    play2.click(function(){
-       if(btn==true){
-             clearInterval(setIntervalId2);
-          
-          $(this).attr('Class','start2')
-          btn=false;
-       }else{
-          timer1();
-          $(this).attr('Class','stop2')
-          btn=true;
-       }
-     });
-	 
-	 $(".pop_ctrl2 .prev2, .pop_ctrl2 .next2 ").hover(function(){
-		 clearInterval(setIntervalId2);
-	},function(){
-		 timer1();
-	});
-	 
-	 timer1();
-	function timer1(){
-      setIntervalId2=setInterval(function(){
-        var prev2=banners.eq(current1);
+    //배너 화살표 스크롤 
+    $('.scroll a').click(function(){
+      var $elem = $($(this).attr("href"));
+      var offset = $elem.offset().top - ($(window).height() / 2) + ($elem.height() / 2);
+    $('html, body').animate({
+      scrollTop: offset
+    }, 600);
+    return false;
+  });
 
-        move1(prev2,0,"-100%");
-        current1++;  
-        if(current1==banners.size()){current1=0;}
-        var next2=banners.eq(current1);
-   
-        move1(next2,'100%',0);
-      },2000);
-    }
-	function move1(tg, start, end){
-      tg.css("left",start).stop().animate({left:end});
-    }
-    rightBtns.click(function(){
-       var prev2=banners.eq(current1);
-
-        move1(prev2,0,"-100%");
-        current1++;  
-        if(current1==banners.size()){current1=0;}
-        var next2=banners.eq(current1);
-   
-        move1(next2,'100%',0);
-    });
-    leftBtns.click(function(){
-       var prev2=banners.eq(current1);
-
-		move1(prev2,0,'100%');
-		current1--;
-      
-		if(current1==-banners.size()){current1=0;}
-		var next2=banners.eq(current1);
-		move1(next2,'-100%',0);  
-     });
-
-     function move1(tg, start, end){
-      tg.css("left",start).stop().animate({left:end});
-    }
-    rightBtns.click(function(){
-       var prev2=banners.eq(current1);
-
-        move1(prev2,0,"-100%");
-        current1++;  
-        if(current1==banners.size()){current1=0;}
-        var next2=banners.eq(current1);
-   
-        move1(next2,'100%',0);
-    });
-    leftBtns.click(function(){
-       var prev2=banners.eq(current1);
-
-		move1(prev2,0,'100%');
-		current1--;
-      
-		if(current1==-banners.size()){current1=0;}
-		var next2=banners.eq(current1);
-		move1(next2,'-100%',0);  
-     });
-
-
-     function move2(tg, start, end){
-      tg.css("left",start).stop().animate({left:end});
-    }
-    boxleft.click(function(){
-       var prev3=box5.eq(current2);
-       var prevNum=num2.eq(current2);
-        move2(prev3,0,"100%");
-        prevNum.removeClass('on');
-        current2--;
  
+    //스크롤할때 자연스럽게
+    function isElementUnderBottom(elem, triggerDiff) {
+      const { top } = elem.getBoundingClientRect();
+      const { innerHeight } = window;
+      return top > innerHeight + (triggerDiff || 0);
+    }
+    
+    function handleScroll() {
+      const elems = document.querySelectorAll('.up-on-scroll');
+      elems.forEach(elem => {
+        if (isElementUnderBottom(elem, -20)) {
+          elem.style.opacity = "0";
+          elem.style.transform = 'translateY(70px)';
+        } else {
+          elem.style.opacity = "1";
+          elem.style.transform = 'translateY(0px)';
+        }
+      })
+    }
+    
+    window.addEventListener('scroll', handleScroll);
 
-        if(current2==-box5.size()){current2=0;}
-        var next3=box5.eq(current2);
-        var nextNum=num2.eq(current2);
-        
-      
-        move2(next3,'100%',0);
-        nextNum.addClass('on');
-    });
-    boxright.click(function(){
-       var prev3=box5.eq(current2);
-       var prevNum=num2.eq(current2);
-		move2(prev3,0,'-100%');
-    prevNum.removeClass('on');
-    current2++; 
-      
-		if(current2==box5.size()){current2=0;}
-    var next3=box5.eq(current2);
-    var nextNum=num2.eq(current2);
-    move2(next3,'100%',0);  
-    nextNum.addClass('on');
-     }); 
 });
